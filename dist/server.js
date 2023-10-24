e5572744ac60a730568cdad5b98d0ddad258615e
YanelPH <yphinerahorth@gmail.com>
2023-10-24 15:59:25 +0200

    fix

--- dist/server.js
+++ dist/server.js
@@ -4,23 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
 const config_1 = __importDefault(require("./config/config"));
 const PORT = config_1.default.port;
 const app = (0, express_1.default)(); //Express is a minimalist, flexible Node.js Web application infrastructure
-//const list = require("express-list-endpoints"); //Express endpoint parser to retrieve a list of the passed router with the set verbs
-//app.use(helmet()); //Helmet helps secure Express apps by setting HTTP response headers
-//app.use(cors()); //Cross-Origin Resource Sharing (CORS) is a protocol that enables scripts running on a browser client to interact with resources from a different origin
-//app.use(express.json()); //It parses incoming requests with JSON payloads and is based on body-parser
-//app.use(express.urlencoded({ extended: true })); //It parses incoming requests with URL-encoded payloads and is based on a body parser
+const list = require("express-list-endpoints"); //Express endpoint parser to retrieve a list of the passed router with the set verbs
+app.use((0, helmet_1.default)()); //Helmet helps secure Express apps by setting HTTP response headers
+app.use((0, cors_1.default)()); //Cross-Origin Resource Sharing (CORS) is a protocol that enables scripts running on a browser client to interact with resources from a different origin
+app.use(express_1.default.json()); //It parses incoming requests with JSON payloads and is based on body-parser
+app.use(express_1.default.urlencoded({ extended: true })); //It parses incoming requests with URL-encoded payloads and is based on a body parser
 //await connectDb();
 //Router list
 //app.use("/users", userRoutes);
 //Show express-list-endpoints in the mainpage
-/*app.get("/", (req: Request, res: Response) => {
-    res.send(list(app));
-  });*/
 app.get("/", (req, res) => {
-    res.send("Welcome to my server");
+    res.send(list(app));
 });
 //The app.listen() function is used to bind and listen to the connections on the specified host and port.
 app.listen(PORT, () => {
--- src/server.ts
+++ src/server.ts
@@ -8,24 +8,22 @@ import connectDb from "./config/connectToMongo";
 
 const PORT = config.port;
 const app = express(); //Express is a minimalist, flexible Node.js Web application infrastructure
-//const list = require("express-list-endpoints"); //Express endpoint parser to retrieve a list of the passed router with the set verbs
+const list = require("express-list-endpoints"); //Express endpoint parser to retrieve a list of the passed router with the set verbs
 
-//app.use(helmet()); //Helmet helps secure Express apps by setting HTTP response headers
-//app.use(cors()); //Cross-Origin Resource Sharing (CORS) is a protocol that enables scripts running on a browser client to interact with resources from a different origin
-//app.use(express.json()); //It parses incoming requests with JSON payloads and is based on body-parser
-//app.use(express.urlencoded({ extended: true })); //It parses incoming requests with URL-encoded payloads and is based on a body parser
+app.use(helmet()); //Helmet helps secure Express apps by setting HTTP response headers
+app.use(cors()); //Cross-Origin Resource Sharing (CORS) is a protocol that enables scripts running on a browser client to interact with resources from a different origin
+app.use(express.json()); //It parses incoming requests with JSON payloads and is based on body-parser
+app.use(express.urlencoded({ extended: true })); //It parses incoming requests with URL-encoded payloads and is based on a body parser
 
 //await connectDb();
 //Router list
 //app.use("/users", userRoutes);
 
 //Show express-list-endpoints in the mainpage
-/*app.get("/", (req: Request, res: Response) => {
-    res.send(list(app));
-  });*/
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to my server");
+  res.send(list(app));
 });
+
 //The app.listen() function is used to bind and listen to the connections on the specified host and port.
 app.listen(PORT, () => {
   console.log(`Server started correctly on port ${PORT}`);
