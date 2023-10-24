7314de54aedae06d82f44101d1165d9131614250
YanelPH <yphinerahorth@gmail.com>
2023-10-24 15:35:55 +0200

    fix

--- dist/server.js
+++ dist/server.js
@@ -13,17 +13,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const helmet_1 = __importDefault(require("helmet"));
-const cors_1 = __importDefault(require("cors"));
 const config_1 = __importDefault(require("./config/config"));
 const initApp = () => __awaiter(void 0, void 0, void 0, function* () {
     const PORT = config_1.default.port;
     const app = (0, express_1.default)(); //Express is a minimalist, flexible Node.js Web application infrastructure
     const list = require("express-list-endpoints"); //Express endpoint parser to retrieve a list of the passed router with the set verbs
-    app.use((0, helmet_1.default)()); //Helmet helps secure Express apps by setting HTTP response headers
-    app.use((0, cors_1.default)()); //Cross-Origin Resource Sharing (CORS) is a protocol that enables scripts running on a browser client to interact with resources from a different origin
-    app.use(express_1.default.json()); //It parses incoming requests with JSON payloads and is based on body-parser
-    app.use(express_1.default.urlencoded({ extended: true })); //It parses incoming requests with URL-encoded payloads and is based on a body parser
+    //app.use(helmet()); //Helmet helps secure Express apps by setting HTTP response headers
+    //app.use(cors()); //Cross-Origin Resource Sharing (CORS) is a protocol that enables scripts running on a browser client to interact with resources from a different origin
+    //app.use(express.json()); //It parses incoming requests with JSON payloads and is based on body-parser
+    //app.use(express.urlencoded({ extended: true })); //It parses incoming requests with URL-encoded payloads and is based on a body parser
     //await connectDb();
     //Router list
     //app.use("/users", userRoutes);
--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,10 @@ const initApp = async () => {
   const app = express(); //Express is a minimalist, flexible Node.js Web application infrastructure
   const list = require("express-list-endpoints"); //Express endpoint parser to retrieve a list of the passed router with the set verbs
 
-  app.use(helmet()); //Helmet helps secure Express apps by setting HTTP response headers
-  app.use(cors()); //Cross-Origin Resource Sharing (CORS) is a protocol that enables scripts running on a browser client to interact with resources from a different origin
-  app.use(express.json()); //It parses incoming requests with JSON payloads and is based on body-parser
-  app.use(express.urlencoded({ extended: true })); //It parses incoming requests with URL-encoded payloads and is based on a body parser
+  //app.use(helmet()); //Helmet helps secure Express apps by setting HTTP response headers
+  //app.use(cors()); //Cross-Origin Resource Sharing (CORS) is a protocol that enables scripts running on a browser client to interact with resources from a different origin
+  //app.use(express.json()); //It parses incoming requests with JSON payloads and is based on body-parser
+  //app.use(express.urlencoded({ extended: true })); //It parses incoming requests with URL-encoded payloads and is based on a body parser
 
   //await connectDb();
   //Router list
